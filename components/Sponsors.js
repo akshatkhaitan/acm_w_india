0ec3be762cf0ec412e7e17b582138fc5ae294a72
Akshat Khaitan <khaitanakshat40@gmail.com>
2022-09-23 11:31:53 +0530

    Sponsors Image Size Updated

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -8,30 +8,39 @@ import pic5 from "../assets/im_5.png";
 import pic6 from "../assets/im_6.png";
 import pic7 from "../assets/im_7.png";
 import Image from "next/image";
-
+ 
 function Sponsors() {
 	const data = [
-		{ img: pic1, id: 1 },
-		{ img: pic2, id: 2 },
-		{ img: pic3, id: 3 },
-		{ img: pic7, id: 4 },
-		{ img: pic4, id: 5 },
-		{ img: pic5, id: 6 },
-		{ img: pic6, id: 7 },
+		{ img: pic1, id: 1 ,h:152,w:600},
+		{ img: pic2, id: 2 ,h:400,w:600},
+		{ img: pic3, id: 3 ,h:156,w:600},
+		{ img: pic7, id: 4 ,h: 600,w:600},
+		{ img: pic4, id: 5 ,h:205,w:600},
+		{ img: pic5, id: 6 ,h:247.45,w:600},
+		{ img: pic6, id: 7 ,h:240,w:600},
 	];
 	return (
 		<>
-			<div className={styles.sponsors_outer}>
+			<div vertical layout className={styles.sponsors_outer}   >
+			 
 				{data.map((com) => {
 					return (
-						<div key={com.id}>
-							<Image src={com.img} height={100} width={200} />
-						</div>
+						
+						<div vertical layout key={com.id}  >
+							<Image src={com.img}
+							width={com?.w}
+							height={com?.h}
+							/>
+						</div>	
+						
+ 
 					);
+					
+					
 				})}
 			</div>
 		</>
 	);
 }
-
+ 
 export default Sponsors;
