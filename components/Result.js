225cc44c745666ec24394e865a6313c78fbf225c
Ankesh Pandey <77636343+anvaishy@users.noreply.github.com>
2022-09-25 13:57:00 +0530

    Results Page updated

--- components/Result.js
+++ components/Result.js
@@ -4,12 +4,111 @@ import styles from "../styles/Home.module.css";
 function Result() {
 	return (
 		<>
-			<div className={styles.outer}>
-				<h1 className={styles.h1}>Have Patience !!</h1>
-				<h2 className={styles.h2}>
-					Results will be announced on 8th October, 2022
-				</h2>
-			</div>
+			<div className={styles.wrapper}>
+			<table className={styles.table}>
+				<thead className={styles.head}>
+					<tr>
+						<td data-title="DATE">Name</td>
+						{/* <td data-title="NAME OF THE EVENT">Position </td> */}
+						<td data-title="NAME OF EXPERTS">College</td>
+					</tr>
+				</thead>
+				<tbody>
+					<br />
+					<tr>
+						<td data-title="DATE">Prayushi Faldu</td>
+						{/* <td data-title="NAME OF THE EVENT">Chair </td> */}
+						<td data-title="NAME OF EXPERTS">Indian Institute of Technology Delhi</td>
+					</tr>
+					<br />
+					<tr>
+						<td data-title="DATE">Mallika Prabhakar</td>
+						{/* <td data-title="NAME OF THE EVENT">Head </td> */}
+						<td data-title="NAME OF EXPERTS">Indian Institute of Technology Delhi</td>
+					</tr>
+					<br />
+					<tr>
+						<td data-title="DATE">Sudha S</td>
+						{/* <td data-title="NAME OF THE EVENT">Head </td> */}
+						<td data-title="NAME OF EXPERTS"> IIT Madras</td>
+					</tr>
+					<br />
+					<tr>
+						<td data-title="DATE">Mrunmayi Bhalerao</td>
+						{/* <td data-title="NAME OF THE EVENT"> */}
+						{/* Faculty Sponsor (OIST ACM Student Chapter){" "} */}
+						{/* </td> */}
+						<td data-title="NAME OF EXPERTS">Indian Institute of Technology, Delhi</td>
+					</tr>
+					<br />
+					<tr>
+						<td data-title="DATE">Lakshmi Nikhitha Atyam</td>
+						{/* <td data-title="NAME OF THE EVENT"> */}
+						{/* Faculty Sponsor (OIST ACM Student Chapter){" "} */}
+						{/* </td> */}
+						<td data-title="NAME OF EXPERTS">Indian Institute of Technology Palakkad</td>
+					</tr><br />
+					<tr>
+						<td data-title="DATE">Amritha P P</td>
+						{/* <td data-title="NAME OF THE EVENT"> */}
+						{/* Faculty Sponsor (OIST ACM Student Chapter){" "} */}
+						{/* </td> */}
+						<td data-title="NAME OF EXPERTS">Thiagarajar College of Engineering</td>
+					</tr><br />
+					<tr>
+						<td data-title="DATE">Vanshita Garg</td>
+						{/* <td data-title="NAME OF THE EVENT"> */}
+						{/* Faculty Sponsor (OIST ACM Student Chapter){" "} */}
+						{/* </td> */}
+						<td data-title="NAME OF EXPERTS">Indian Institute of Technology Kharagpur</td>
+					</tr><br />
+					<tr>
+						<td data-title="DATE">Talika Gupta</td>
+						{/* <td data-title="NAME OF THE EVENT"> */}
+						{/* Faculty Sponsor (OIST ACM Student Chapter){" "} */}
+						{/* </td> */}
+						<td data-title="NAME OF EXPERTS"></td>
+					</tr><br />
+					<tr>
+						<td data-title="DATE">Kavya Chopra</td>
+						{/* <td data-title="NAME OF THE EVENT"> */}
+						{/* Faculty Sponsor (OIST ACM Student Chapter){" "} */}
+						{/* </td> */}
+						<td data-title="NAME OF EXPERTS">Indian Institute of Technology, Delhi</td>
+					</tr><br />
+					<tr>
+						<td data-title="DATE">Khooshrin Pithawalla</td>
+						{/* <td data-title="NAME OF THE EVENT"> */}
+						{/* Faculty Sponsor (OIST ACM Student Chapter){" "} */}
+						{/* </td> */}
+						<td data-title="NAME OF EXPERTS">Birla Institute of Technology and Science, Pilani - Hyderabad Campus</td>
+					</tr><br />
+					<tr>
+						<td data-title="DATE">Keerthana Nevuri</td>
+						{/* <td data-title="NAME OF THE EVENT"> */}
+						{/* Faculty Sponsor (OIST ACM Student Chapter){" "} */}
+						{/* </td> */}
+						<td data-title="NAME OF EXPERTS">University college of engineering, Osmania university </td>
+					</tr><br />
+					<tr>
+						<td data-title="DATE">Gayatri Munde</td>
+						{/* <td data-title="NAME OF THE EVENT"> */}
+						{/* Faculty Sponsor (OIST ACM Student Chapter){" "} */}
+						{/* </td> */}
+						<td data-title="NAME OF EXPERTS">BRACT's Vishwakarma Institute of Technology, Pune</td>
+					</tr><br />
+					<tr>
+						<td data-title="DATE">Ojasvi Bansal</td>
+						{/* <td data-title="NAME OF THE EVENT"> */}
+						{/* Faculty Sponsor (OIST ACM Student Chapter){" "} */}
+						{/* </td> */}
+						<td data-title="NAME OF EXPERTS">Indian Institute of Technology, Delhi</td>
+					</tr><br />
+					<br />
+				</tbody>
+				<br />
+			</table>
+		</div>
 		</>
 	);
 }
